ad82bed8abdde51878536667edc7b08c0f2a8afa
shumilovas <terminatorson@gmail.com>
2025-12-14 13:45:41 +0300

    закончил расчёты в r, нужно теперь всё переносить в яву, а потом в anylogic

--- rstudio/heigt_profile.R
+++ rstudio/heigt_profile.R
@@ -1,3 +1,6 @@
 getwd()
 setwd("C:/Users/W10/Documents/GitHub/height_profile/rstudio")
-read.csv2("height_profile.csv")
+height_profile = read.csv("height_profile.csv")
+height = height_profile$Height
+decrease = 13+20*log10(height)
+
